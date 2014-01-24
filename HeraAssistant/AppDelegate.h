416342b07c1c7f11cba1a4499f69a5212e164e9c
magicvoid <magicvoid@me.com>
2014-01-24 12:29:19 +0800

    Initial Commit

---
+++ HeraAssistant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeraAssistant
+//
+//  Created by xChen on 14-1-24.
+//  Copyright (c) 2014年 sintn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeraAssistant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeraAssistant
+//
+//  Created by xChen on 14-1-24.
+//  Copyright (c) 2014年 sintn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
